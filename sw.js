049879d94d1846246492a047f2b63b4f04e67f3c
kateghal <kghaley2004@gmail.com>
2026-08-14 09:40:33 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
-const CACHE='chud-tracker-v5';
+const CACHE='chud-tracker-v8';
 const APP_SHELL=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.mode==='navigate'){
-    event.respondWith(fetch(req,{cache:'no-store'}).then(res=>{
-      const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return res;
-    }).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./'))));
-    return;
-  }
-  event.respondWith(fetch(req).then(res=>{
-    if(req.method==='GET'&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));}
-    return res;
-  }).catch(()=>caches.match(req)));
-});
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const req=event.request;if(req.mode==='navigate'){event.respondWith(fetch(req,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./'))));return}event.respondWith(fetch(req).then(res=>{if(req.method==='GET'&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy))}return res}).catch(()=>caches.match(req)))});
